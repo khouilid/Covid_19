fcf8e76326475221bcb34b6ce9763e219e1851b0
khouilid <57531206+khouilid@users.noreply.github.com>
2020-04-01 17:51:43 +0200

    i finish the page Accueil ()

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,7 @@ const gulp = require("gulp");
 const sass = require('gulp-sass');
 let cleanCSS = require('gulp-clean-css');
 const uglifycss = require('gulp-uglifycss');
-// const watch = require('gulp-watch');
-
-
+const watch = require('gulp-watch');
 gulp.task('sass', function () {
     return gulp.src('./sass/main.scss')
       .pipe(sass().on('error', sass.logError))
@@ -20,14 +18,10 @@ gulp.task('minify-css', () => {
  .pipe(gulp.dest('dist'));
 });
 
-// We create a 'default' task that will run when we run `gulp` in the project
-gulp.task('default', function() {
-// We use `gulp.watch` for Gulp to expect changes in the files to run again
-  gulp.watch('./sass/main.scss', function(evt) {
-  gulp.task('sass');
-  });
-  gulp.watch('./style/*.css', function(evt) {
-    gulp.task('minify-css');
-    });
-});
+gulp.task('watch', function(){
+  gulp.watch('./sass/main.scss', gulp.series(['sass'])); 
+  // Other watchers
+  gulp.watch('./style', gulp.series(['minify-css']));
+})
+
 
